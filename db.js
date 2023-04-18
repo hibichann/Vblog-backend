04cddf7ef4048399905e7365aca89707c63fc267
hibi01 <coder101011@outlook.com>
2023-04-18 13:44:05 +0800

    commit

--- db.js
+++ db.js
@@ -10,12 +10,12 @@ let pool = mysql.createPool({
 	// timezone: "utc", //大坑，必须加这一句，否则时间不对
 })
 //封装数据库sql请求操作，返回的是一个包含对象的数组
-let Query = (sql) => {
+let Query = (sql, params) => {
 	return new Promise(function (resolve, reject) {
 		//从连接池中拿一条链接
 		pool.getConnection(function (err, connection) {
 			if (err) return reject(err)
-			connection.query(sql, function (error, res) {
+			connection.query(sql, params, function (error, res) {
 				// console.log(res);
 				connection.release()
 				if (error) {
@@ -29,4 +29,5 @@ let Query = (sql) => {
 
 module.exports = {
 	Query,
+	pool,
 }
--- sev/save.js
+++ sev/save.js
@@ -1,9 +1,24 @@
 const db = require("../db.js")
 let postArt = async (req) => {
-	console.log(req)
-	let sql = `INSERT INTO blog (title, content, class, lang) VALUES (:title,:content,:cate,:lang);`
-	let res = await db.Query(sql, req)
-	console.log(res)
+	const { title, content, cate, lang } = req
+	const art = { title, content, class: cate, lang }
+	await db.Query(
+		"INSERT INTO blog SET ?",
+		[art],
+		function (error, results, fields) {
+			console.log(error, results, fields)
+		}
+	)
+	const result = await db.Query(
+		"SELECT * FROM blog WHERE id = LAST_INSERT_ID()"
+	)
+	let values = req.tagarr
+		.map((tagid) => {
+			return `(${result[0].id}, ${tagid})`
+		})
+		.join(", ")
+	let sql = `INSERT INTO tag_blog (blogid, tagid) VALUES ${values};`
+	await db.Query(sql)
 	return {}
 }
 module.exports = { postArt }
